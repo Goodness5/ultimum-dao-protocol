dfbc6e16a0dde53bd445b31a30634353ce6a93a8
mrpatrick030 <patrickmaebi@gmail.com>
2024-04-29 19:46:00 +0100

    integration almost complete

--- front-end-and-integration/pages/dashboard.js
+++ front-end-and-integration/pages/dashboard.js
@@ -118,7 +118,7 @@ export default function Dashboard(){
   return (
     <>
     <Head>
-   <title>Ultimum Protocol Dashboard - Acess to Ultimum's Ecosystem (DAO, Swap, Staking, Token, Treasury, Lending/Borrowing)</title>
+   <title>Ultimum Protocol Dashboard - Acess to Ultimum's Ecosystem (DAO, Swap, Staking, Token, Treasury, NFT, Lending/Borrowing)</title>
    <link rel="shortcut icon" href="/favicon.ico" />
    </Head>
    <div>
